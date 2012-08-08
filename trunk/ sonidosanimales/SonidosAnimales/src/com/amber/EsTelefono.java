fd112ad852e4999d115e8d369101c1fdd63eae59
jfambros <jfambros@425ccfba-6b57-00c3-d940-37b15bf852ad>
2012-08-08 20:53:28 +0000

    Inicio guardar

--- trunk/ sonidosanimales/SonidosAnimales/src/com/amber/EsTelefono.java
+++ trunk/ sonidosanimales/SonidosAnimales/src/com/amber/EsTelefono.java
@@ -1,5 +1,15 @@
 package com.amber;
 
-public class EsTelefono {
+import android.content.Context;
+import android.telephony.TelephonyManager;
 
+public class EsTelefono {
+		public boolean verifica(Context context){
+			TelephonyManager manager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+	        if(manager.getPhoneType() == TelephonyManager.PHONE_TYPE_NONE){
+	            return false;
+	        }else{
+	            return true;
+	        }
+		}
 }
